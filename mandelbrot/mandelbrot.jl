ac7c67aa41a94a81a5b46b6620d809adf07d5860
mkcq <40498101+mkcq@users.noreply.github.com>
2024-04-21 18:20:09 +0200

    mpi and separate startup file

--- mandelbrot/mandelbrot.jl
+++ mandelbrot/mandelbrot.jl
@@ -1,4 +1,72 @@
+# using MPIClusterManagers
+# using Distributed
+using GLMakie
+using LinearAlgebra
+using MPI
+
+function mandel(x, y, max_iters)
+    z = Complex(x, y)
+    c = z
+    threshold = 2
+    for n in 1:max_iters
+        if abs(z) > threshold
+            return n - 1
+        end
+        z = z^2 + c
+    end
+    max_iters
+end
+
+function plotMandel()
+    xAxis = LinRange(xmin, xmax, GRID_RESOLUTION)
+    yAxis = LinRange(ymin, ymax, GRID_RESOLUTION)
+    data = [mandel(x, y, MAX_ITERS) for x in xAxis, y in yAxis]
+    p = heatmap(data)
+    save("mandelbrot2.png", p)
+end
+
+MPI.Init()
+comm = MPI.Comm_dup(MPI.COMM_WORLD)
+nranks = MPI.Comm_size(comm)
+rank = MPI.Comm_rank(comm)
+size = MPI.Comm_size(comm)
+# println("Rank $rank of ranks $nranks ")
+
+ROOT = 0
+GRID_RES = 1000
+MAX_ITER = 100
+xmin = -1.7; xmax = 0.7; ymin = -1.2; ymax = 1.2
+
+# Divide rows to calculate by number of workers
+rows_w = GRID_RES ÷ nranks + (GRID_RES % nranks > rank)
+lb = 1 + (rank - 1) * rows_w + rows_w
+ub = rank * rows_w + rows_w
+# println("Rank $rank computes [$lb:$ub]\n")
+
+cols = LinRange(ymin, ymax, GRID_RES)
+xaxis = LinRange(xmin, xmax, GRID_RES)
+rows = LinRange(xaxis[lb], xaxis[ub], rows_w)
+# println("Rank $rank computes cols = $cols and rows = $rows\n")
+
+snd = [mandel(r, c, MAX_ITER) for r in rows, c in cols]
+rcv = MPI.Gather(snd, comm;root=ROOT)
+
+if rank == ROOT
+    temp = rcv
+    m = Matrix{Int64}(undef, GRID_RES, GRID_RES)
+    for (i, v) in enumerate(temp)
+        m[i] = v
+    end
+    tm = transpose(m)
+    p = heatmap(tm)
+    save("mandelbrot2.png", p)
+end
+
 #=
+Citations/References:
+
+Simon Byrne, Lucas C. Wilcox, and Valentin Churavy (2021) "MPI.jl: Julia bindings for the Message Passing Interface". JuliaCon Proceedings, 1(1), 68, doi: 10.21105/jcon.00068
+
 Exercise 3 from: https://www.francescverdugo.com/XM_40017/dev/julia_basics/
 
 Function mandel estimates if a given point (x,y) in the complex plane belongs to the Mandelbrot set.
@@ -20,89 +88,3 @@ GLMakie is a GPU-accelerated plotting back-end for Julia.
 It is a large package and it can take some time to install and to generate the first plot.
 Be patient.
 =#
-
-using LinearAlgebra
-using MPIClusterManagers
-using Distributed
-
-NUM_RANKS = 4
-
-# Create workers that can call MPI functions
-if procs() == workers()
-    manager = MPIWorkerManager(NUM_RANKS)
-    addprocs(manager)
-end
-
-@everywhere workers() begin
-    using MPI
-    using GLMakie
-
-    function mandel(x, y, max_iters)
-        z = Complex(x, y)
-        c = z
-        threshold = 2
-        for n in 1:max_iters
-            if abs(z) > threshold
-                return n - 1
-            end
-            z = z^2 + c
-        end
-        max_iters
-    end
-
-    MPI.Init()
-    comm = MPI.Comm_dup(MPI.COMM_WORLD)
-    nranks = MPI.Comm_size(comm)
-    rank = MPI.Comm_rank(comm)
-    # println("Hello, I am process $rank of $nranks processes!")
-    root = 0
-
-    GRID_RESOLUTION = 1000
-    MAX_ITERS = 100
-    xmin = -1.7
-    xmax = 0.7
-    ymin = -1.2
-    ymax = 1.2
-    
-    # Divide rows to calculate by number of workers
-    nRowsR = GRID_RESOLUTION ÷ nranks
-    lb = 1 + (myid() - 2) * nRowsR
-    ub = (myid() - 1) * nRowsR
-    # println("Rank $rank will compute rows $lb until $ub")
-
-    cols = LinRange(ymin, ymax, GRID_RESOLUTION)
-    xAxis = LinRange(xmin, xmax, GRID_RESOLUTION)
-    rows = LinRange(xAxis[lb], xAxis[ub], nRowsR)
-    snd = [mandel(r, c, MAX_ITERS) for r in rows, c in cols]
-    
-    # @show typeof(snd), size(snd)
-    
-    # rcv = MPI.Gather(snd, comm;root)
-
-    # MPI.Barrier(comm)
-
-    # type of rcv should be a matrix Matrix{Int64} of (1000, 1000)
-
-    if rank == root
-        global res = snd
-        # @show nranks
-        rcv = Ref(0)
-        @show typeof(res), size(res)
-        
-        MPI.Recv!(rcv, comm;source=1, tag=0)
-        res = vcat(res, rcv)
-        @show typeof(res), size(res)
-
-        # for i in 1:(nranks-1)
-        #     res = vcat(res, MPI.Recv!(rcv, comm;source=i, tag=0)) 
-        # end
-        @show typeof(res), size(res)
-
-        # @show typeof(rcv)
-        # @show size(rcv)
-        # p = heatmap(rcv)
-        # save("mandelbrot2.png", p)
-    else
-        MPI.Send(snd, comm;dest=root, tag=0)
-    end
-end
