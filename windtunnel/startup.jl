ff0ade98b11f3369910b83cd620790cb6b6bb4fc
mkcq <40498101+mkcq@users.noreply.github.com>
2024-05-02 15:35:40 +0200

    main additions

--- windtunnel/startup.jl
+++ windtunnel/startup.jl
@@ -1,6 +1,6 @@
 using MPI
 
-PROCS = [1, 2, 4]
+PROCS = [4]
 FILE = "wind.jl"
 timings = [;]
 
@@ -10,3 +10,36 @@ for (i, v) in enumerate(PROCS)
     push!(timings, (v, t))
 end
 display(timings)
+
+#=
+#!/bin/bash
+#SBATCH --time=00:15:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=16
+
+. /etc/profile.d/lmod.sh
+module load openmpi/gcc/64
+. /etc/profile.d/lmod.sh
+module load julia/1.7.3
+
+pwd
+
+# Parallel
+
+mpiexec -np 4 julia --project=. -e '
+    include("wind.jl")
+    rows = 1
+    cols = 1
+    max_iter = 1
+    threshold = 1
+    main(rows, cols, max_iter, threshold)
+'
+=#
+
+#=
+main(rows, cols, max_iter, var_threshold, inlet_pos, inlet_size, 
+    particles_f_band_pos, particles_f_band_size, particles_f_density, 
+    particles_m_band_pos, particles_m_band_size, particles_m_density,
+    rand_seq_1, rand_seq_2, rand_seq_3, 
+    particle_row, particle_col, particle_density, ...)
+=#
--- windtunnel/wind.jl
+++ windtunnel/wind.jl
@@ -5,45 +5,116 @@ STEPS = 8
 
 # Structure to represent a solid particle in the tunnel surface.
 struct Particle
-    position::Vector
+    position::Vector # As [column, row]
     mass::Int
     resistance::Int
-    speed::Vector
+    speed::Vector # Two dimensional velocity vector.
     oldFlow::Int
 end
 
-function main()
-    max_iter = 1
+# TODO: Function to get wall time.
+# TODO: Function to simplify matrix/2D array access.
+# TODO: Update flow in a matrix position.
+# TODO: Move particle function.
+# TODO: Print the current state of the simulation.
+# TODO: Print usage line in stderr.
+# TODO: Start global timer
+# TODO: Parallel initialization.
+# TODO: Simulation.
+# TODO: Particle movement each STEPS iterations.
+# TODO: Effects due to particles each STEPS iterations.
+# TODO: Propagation stage.
+# TODO: MPI: Fill result arrays used for later output.
+# TODO: Stop global timer.
+# TODO: Output for the leaderboard.
+
+function printArguments(rows, cols, max_iter, threshold, fan_pos, fan_size,
+    fixed_band_pos, fixed_band_size, fixed_density, 
+    moving_band_pos, moving_band_size, moving_density, 
+    n_particles)
+    println("Rows: $rows, Columns: $cols, Max iter: $max_iter, Threshold: $threshold\n
+    Fan: $fan_pos, $fan_size\n
+    Fixed particles : $fixed_band_pos $fixed_band_size $fixed_density\n
+    Moving particles: $moving_band_pos $moving_band_size $moving_density\n
+    Number of particles: $n_particles")
 end
 
-# TODO: Function to get wall time.
+"""For DAS the arguments are passed as parameters to main().
+For local execution the arguments are in the command line.
 
-# TODO: Function to simplify matrix/2D array access.
+Order of arguments: rows, cols, max_iter, threshold, fan_pos, fan_size, 
+fixed_band_pos, fixed_band_size, fixed_density, 
+moving_band_pos, moving_band_size, moving_density, 
+rand_seq_1, rand_seq_2, rand_seq_3, 
+particle_row, particle_col, particle_density, ...
+"""
+function main()
+    
+    # Simulation data.
+    rows = parse(Int, ARGS[1])          # Tunnel rows.
+    cols = parse(Int, ARGS[2])          # Tunnel columns.
+    max_iter = parse(Int, ARGS[3])      # Max number of sim (simulation) steps.
+    threshold = parse(Float16, ARGS[4]) # Threshold of variability to continue the sim.
+    fan_pos = parse(Int, ARGS[5])       # First position of the fan.
+    fan_size = parse(Int, ARGS[6])      # Fan size.
+    fixed_band_pos = parse(Int, ARGS[7])        # First position of the band where FIXED particles Start.
+    fixed_band_size = parse(Int, ARGS[8])       # Size of the band where FIXED particles start.
+    fixed_density = parse(Float16, ARGS[9])     # Density of starting FIXED particles.
+    moving_band_pos = parse(Int, ARGS[10])      # First position of the band where MOVING particles start.
+    moving_band_size = parse(Int, ARGS[11])     # Size of the band where MOVING particles start.
+    moving_density = parse(Float16, ARGS[12])   # Density of starting MOVING particles.
 
-# TODO: Update flow in a matrix position.
+    flow = []           # Tunnel air flow.
+    flow_copy = []      # Tunnel air flow (ancillary, of secondary importance, copy).
+    particle_locs = []  # Quickly locate particle positions.
 
-# TODO: Move particle function.
+    # TODO: random seq init ARGS(13 - 15)
 
-# TODO: Print the current state of the simulation.
+    # Initialize MPI basics.
+    MPI.Init()
+    comm = MPI.Comm_dup(MPI.COMM_WORLD)
+    n_ranks = MPI.Comm_size(comm)
+    rank = MPI.Comm_rank(comm)
 
-# TODO: Print usage line in stderr.
+    # Initialize MPI cartesian grid.
+    dims = getDims(n_ranks)             # TODO: create getDims(n_ranks)
+    periodic = map(_ -> false, dims)
+    reorder = false
+    comm_cart = MPI.Cart_create(comm, dims; periodic, reorder)
 
-# TODO: Main program.
+    # Read particles from the arguments.
+    n_particles = (length(ARGS) - 15) ÷ 3               # Number of particles.
+    particles = Vector{Particle}(undef, n_particles)    # List to store cells information
+    for i in 1:n_particles
+        row = parse(Int, ARGS[16 + i * 3]) * PRECISION
+        col = parse(Int, ARGS[17 + i * 3]) * PRECISION
+        res = parse(Int, ARGS[18 + i * 3]) * PRECISION
+        particles[i] = Particle([row, col], 0, res, [0,0], 0)
+    end
 
-# TODO: Start global timer
+    # TODO: Generate fixed/moving particles in the band.
 
-# TODO: Parallel initialization.
+    if rank == 0
+        printArguments(rows, cols, max_iter, threshold, fan_pos, fan_size,
+            fixed_band_pos, fixed_band_size, fixed_density, 
+            moving_band_pos, moving_band_size, moving_density, n_particles)
+    end
+    MPI.Barrier(comm_cart)
 
-# TODO: Simulation.
+    #=
+    Read sim args
 
-# TODO: Particle movement each STEPS iterations.
 
-# TODO: Effects due to particles each STEPS iterations.
+    start global timer
 
-# TODO: Propagation stage.
+    init.
 
-# TODO: MPI: Fill result arrays used for later output.
+    sim.
+        1. change inlet values
+        2. move particles
+        3. effects due to particles
+        4. copy into ancillary structure
+        5. propagation stage
+    =#
 
-# TODO: Stop global timer.
-
-# TODO: Output for the leaderboard.
+end
